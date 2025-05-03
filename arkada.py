1aa606121641172a51cee5cf1520e9e969c5c02f
FX-AM <tancukandrij98@gmail.com>
2025-05-03 11:57:47 +0300

    Update arkada.py

--- arkada.py
+++ arkada.py
@@ -1,9 +1,9 @@
 import pygame
 pygame.init()
-window=pygame.display.set_mode((750,750))
-
+window = pygame.display.set_mode((750, 750))
 window.fill((158, 213, 0))
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
+
 class Area():
     def __init__(self, x=0, y=0, width=10, height=10, color=(255, 255, 255)):
         self.x = x
@@ -25,57 +25,81 @@ class Area():
 
     def collidepoint(self, x, y):
         return self.rect.collidepoint(x, y)
-class Picture(Area):
-    def __init__(self,filename,x=0,y=0,width=10,height=10):
-        Area.__init__(self,x=x,y=y,width=width,height=height,color=None)
-        self.image=pygame.image.load(filename)
-    def draw(self):
-        window.blit(self.image(self.x, self.y))
-platform_x=200
-platform_y=300
-ball=Picture("tennis-ball-png-picture-1.png", 160, 200, 50, 50)
-platform=Picture("wooden-podium-on-transparent-background-natural-stage-for-product-cosmetic-presentation-mock-up-pedestal-or-platform-for-beauty-products-empty-scene-display-showcase-3d-rendering-png.webp",platform_x,platform_y, 100, 30 )
-#створення ворогів
-start_x=5
-start_y=5
-count=4
-monsters=[]
-for i in range(10):
-    y=start_y+(55*i)
-    x=start_x+(25*i)
-    for j in range(count):
-        enemy=Picture("pngtree-video-video-game-retro-vector-png-image_34458072.png", 50, 50 )
-        monsters.append(enemy)
-        x=x+55
-#        count=count-1
-.
 
-game=True
+class Picture(Area):
+    def __init__(self, filename, x=0, y=0, width=10, height=10):
+        super().__init__(x=x, y=y, width=width, height=height)
+        self.image = pygame.transform.scale(pygame.image.load(filename), (width, height))
+        
+    def draw(self):
+        window.blit(self.image, (self.x, self.y))
+class Label(Area):
+    def self_text (self, text, fsize=12, text_color=(0,0,0)):
+        self.image = pygame.font.Sysfont("werdana", fsize).render("Game over", True,(255,0,0))
+    def draw(self, shift_x=0, shift_y=0,):
+        self.fill()
+        window.blit(self.image, (self.x + shift_x, self.y + shift_y))
+platform_x = 200
+platform_y = 300
+
+# ВАЖЛИВО: ці зображення мають бути в одній папці з .py файлом
+ball = Picture(ball.png, 160, 200, 50, 50)
+platform = Picture(platform.png, platform_x, platform_y, 100, 30)
+
+# створення ворогів
+start_x = 5
+start_y = 5
+monsters = []
+for i in range(4):
+    y = start_y + (55 * i)
+    x = start_x + (25 * i)
+    for j in range(4):
+        enemy = Picture(enemy.png, x, y, 50, 50)
+        monsters.append(enemy)
+        x += 55
+#початкова швидкість м'яча
+speed_x=3
+speed_y=3
+game = True
+
+font1=pygame.font.Font(None, 70)
+font2=pygame.font.Font(None, 30)
+image=pygame.font.Sysfont("werdana", fsize).render("Game over", True,(255,0,0))
 while game:
     window.fill((158, 213, 0))
-#    platform.fill()
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game=False
+        if event.type == pygame.QUIT:
+            game = False
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
-                platform.self_x+=3
+                platform.x+=10
             if event.key==pygame.K_LEFT:
-                platform.self_x-=3
-
+                platform.x=-10
     for m in monsters:
         m.draw()
-    platform.draw
-    ball.draw
-    ball_rect_x+=speed_x
-    ball_rect_y+=speed_y
-    if ball.rect_y<0
+
+    platform.draw()
+    ball.draw()
+    ball.x+=speed_x
+    ball.y+=speed_y
+    if ball.y<0:
         speed_y*=-1
-    if ball.rect_x<0 or ball.rect_x>750
-    speed_x*=-1
-    if ball.rect.collidepoint(platform.rect):
+    if ball.x<0 or ball.x>750:
+        speed_x*=-1
+    if ball.rect.collidepoint(platform_y,platform_y):
         speed_x*=-1
         speed_y*=-1
+    if ball.y>700:
+        time_text=Label(150, 150, 50, 50 (158, 213, 0))
+        time_text.set_text("ти програв", 30, (255, 200, 0))
+        time_text.draw()
+        game=False
+    if len(monsters)==0:
+        time_text=Label(150, 150, 50, 50 (158, 213, 0))
+        time_text.set_text("ти виграв", 30, (255, 200, 0))
+        time_text.draw()
+        game=False
     clock.tick(40)
     pygame.display.update()
+
 pygame.quit()
